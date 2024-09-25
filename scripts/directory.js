1cf4c29311be66e184ac8ac587bb5ff846da2afa
SmartyNotchy <smartynotchy@gmail.com>
2024-09-24 21:35:01 -0400

    Emergency Filepath Fix
    whoopsies!

--- scripts/directory.js
+++ scripts/directory.js
@@ -1,14 +1,14 @@
 const DIRECTORY = {
     "APUSH": {
         "plaintext": "APUSH",
-        "path": "../questions/apush/",
+        "path": "./questions/apush/",
         "files": [
             "unit1", "unit2", "unit3", "unit4", "unit5", "unit6", "unit7", "unit8"
         ]
     },
     "NSL": {
         "plaintext": "AP NSL/AP Gov.",
-        "path": "../questions/nsl/",
+        "path": "./questions/nsl/",
         "files": [
             "u1ch1", "u1ch2", "u1ch3"
         ]
